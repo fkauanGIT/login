2a040456eee83e97e0ba7d80d59ec5141c0ca0ed
fkauanGIT <kauanuniversity@gmail.com>
2025-03-28 11:51:15 -0300

    feat: Using JdbcUserDetailsManager to perform authentication

--- src/main/java/com/app/eazy_bank/config/SecurityConfig.java
+++ src/main/java/com/app/eazy_bank/config/SecurityConfig.java
@@ -5,15 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -30,11 +29,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =User.withUsername("user").password("{noop}123").authorities("read").build();
-        UserDetails adm =User.withUsername("admin").password("{bcrypt}$2a$12$wp4JttoZ346TZYaPXxFjxubQPsQewI91j7y76184WYK3Sykbxu22y").authorities("admin").build();
-
-        return new InMemoryUserDetailsManager(user, adm);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean

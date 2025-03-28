e85447230b41b026a3322ad26f022649a4b397f9
fkauanGIT <kauanuniversity@gmail.com>
2025-03-28 12:11:49 -0300

    feat: Creating JPA Entity and repository classes for new table

--- src/main/java/com/app/eazy_bank/EazyBankApplication.java
+++ src/main/java/com/app/eazy_bank/EazyBankApplication.java
@@ -2,6 +2,8 @@ package com.app.eazy_bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class EazyBankApplication {
